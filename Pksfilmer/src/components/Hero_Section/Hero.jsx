721791a04cef70b92b1628d54d20a280ceafc3e3
Aritra Chakraborty <chakraritra@gmail.com>
2024-08-25 10:18:14 +0530

    removed the unnecessary contents

--- Pksfilmer/src/components/Hero_Section/Hero.jsx
+++ Pksfilmer/src/components/Hero_Section/Hero.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 
 const Hero = () => {
   return (
-        <section className="hero">
+    <section className="hero">
       <h1 className=" text-4xl font-display font-bold leading-snug mt-28 px-24">PKS Photography</h1>
-      <p className="text-red-500">hello everyone</p>
-      <h1> hai ac</h1>
-      <h1>Hello I amm seema contibuting to this project</h1>
-      <h1>Hello I am aritra contributing to this project</h1>
       <p  className="text-xl mt-4 px-24 text-left pr-96 leading-normal mb-16">We photograph the unique moments in your life that make you smile. We provide evergreen memories for our clients to treasure for the rest of their life with our amazing photography. Since 2017, we have been operating as a corporation. However, each of our photographers has documented a trip that goes beyond that. Through our photographs, we want to convey happiness.</p>
     </section>
   );
