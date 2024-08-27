ec180ae168ec5beb02c1df46309ea7f8258a2079
Aritra Chakraborty <chakraritra@gmail.com>
2024-08-28 00:48:39 +0530

    navbar functionalities done

--- Pksfilmer/tailwind.config.js
+++ Pksfilmer/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: { 
       colors: {
-      gold: '#FFD700',
+      primary: '#FFD700',
     },
     fontFamily:{
       'display': ['Poppins', 'sans-serif'],
