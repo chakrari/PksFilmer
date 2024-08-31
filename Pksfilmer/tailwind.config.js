487cca3ccb70edbd5dae8db9ae1de29f16f57d43
Aritra Chakraborty <chakraritra@gmail.com>
2024-08-31 13:09:01 +0530

    conflict resolved

--- Pksfilmer/tailwind.config.js
+++ Pksfilmer/tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: { 
+    extend: {
       colors: {
-      primary: '#FFD700',
+        primary: "#FFD700",
+      },
+      color: {
+        brightColor: "#F4511F",
+        backgroundColor: "#b7bca9",
+        lightText: "#959595",
+      },
+      fontFamily: {
+        display: ["Poppins", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
     },
-    fontFamily:{
-      'display': ['Poppins', 'sans-serif'],
-      'body' : ['Inter', 'sans-serif'],
-    }
-  }
   },
   plugins: [],
-}
-
+};
